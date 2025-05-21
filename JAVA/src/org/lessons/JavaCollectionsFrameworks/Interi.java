1e4da0629bf58f7f15b807504bbec00b5a69204c
alessandroschiano98 <schianoski98@gmail.com>
2025-05-21 21:36:06 +0200

    fix

---
+++ JAVA/src/org/lessons/JavaCollectionsFrameworks/Interi.java
@@ -0,0 +1,17 @@
+package org.lessons.JavaCollectionsFrameworks;
+
+import java.util.ArrayList;
+
+public class Interi {
+    public static void main(String[] args) {
+        // ! AUTOBOXING
+        ArrayList<Integer> interi = new ArrayList<>();
+        interi.add(22);
+        interi.add(33);
+        interi.add(44);
+        System.out.println(interi);
+
+
+    }
+
+}
