de50bb4d63e0208a2531f27cdf021f5b0b36db76
alessandroschiano98 <schianoski98@gmail.com>
2025-06-04 11:29:15 +0200

    FIX

---
+++ JAVA/src/org/lessons/JavaGestioneErrori/Ciclabile.java
@@ -0,0 +1,73 @@
+package org.lessons.JavaGestioneErrori;
+
+public class Ciclabile {
+    private int[] interi;
+    private int indiceCorrente;
+
+    public Ciclabile() {
+        this.indiceCorrente = -1;
+        this.interi = new int[0];
+    }
+
+    public Ciclabile(int[] interi) {
+        this.indiceCorrente = -1;
+        this.interi = interi;
+    }
+
+    public int[] getInteri() {
+        return this.interi;
+    }
+
+    public int getIndiceCorrente() {
+        return this.indiceCorrente;
+    }
+
+    // ! GESTIONE ERRORI
+    public int getElementoSuccessivo() throws Exception {
+        if (hasAncoraElementi()) {
+            this.indiceCorrente = this.indiceCorrente + 1;
+            return this.interi[this.indiceCorrente];
+        } else {
+            throw new Exception("Indice troppo grande per l'array in uso"); // ! da ritornare con un'eccezione
+        }
+    }
+
+    public boolean hasAncoraElementi() {
+        return this.indiceCorrente < this.interi.length - 1;
+    }
+
+    public void addElemento(int nuovoIntero) { // # append
+        // * creo un nuovo array della lunghezza del precedente + 1
+        int[] nuoviInteri = new int[this.interi.length + 1];
+
+        // * copio l'array di prima in quello nuovo
+        for (int i = 0; i < this.interi.length; i++) {
+            nuoviInteri[i] = this.interi[i];
+        }
+
+        // * aggiungo l'ultimo elemento alla fine
+        nuoviInteri[nuoviInteri.length - 1] = nuovoIntero;
+
+        // * sostituisco l'array iniziale con quello nuovo
+        this.interi = nuoviInteri;
+    }
+
+    public void addElementoInTesta(int nuovoIntero) { // $ prepend
+        // & creo un nuovo array della lunghezza del precedente + 1
+        int[] nuoviInteri = new int[this.interi.length + 1];
+
+        // * aggiungo l'ultimo elemento all'inizio
+        nuoviInteri[0] = nuovoIntero;
+
+        // * copio l'array di prima in quello nuovo ad un posto piu' avanti
+        for (int i = 0; i < this.interi.length; i++) {
+            nuoviInteri[i + 1] = this.interi[i];
+        }
+
+        // *  sostituisco l'array iniziale con quello nuovo
+        this.interi = nuoviInteri;
+    }
+
+
+
+}
---
+++ JAVA/src/org/lessons/JavaGestioneErrori/Main.java
@@ -0,0 +1,45 @@
+package org.lessons.JavaGestioneErrori;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inizializzo la mia classe ciclabile");
+        int[] arrayInteri = {55, 66, 77, 88, 99, 0, 1, 3, 55, 1, 2, 34, 44, 3, 52, 3};
+        
+        Ciclabile iteratore = new Ciclabile();
+
+        System.out.println(Arrays.toString(arrayInteri));
+
+        System.out.println(iteratore.getIndiceCorrente());
+
+        try  {
+            System.out.println(iteratore.getElementoSuccessivo());
+        } catch (Exception eccezione){
+            System.out.println("Problemuzzo: " + eccezione.getMessage());
+        }
+
+        System.out.println(Arrays.toString(iteratore.getInteri()));
+
+        iteratore.addElemento(44);
+        iteratore.addElemento(33);
+        iteratore.addElemento(66);
+        iteratore.addElemento(22);
+
+        iteratore.addElementoInTesta(1572);
+        System.out.println(Arrays.toString(iteratore.getInteri()));
+
+
+        while (iteratore.hasAncoraElementi()){
+            try {
+                System.out.println("Elemento al posto: " + (iteratore.getIndiceCorrente() + 1) + ", con valore: " + iteratore.getElementoSuccessivo());
+            } catch (Exception eccezione){
+                System.out.println(eccezione.getMessage());
+            } finally { // ^ IN OGNI CASO esegui questo blocco
+                System.out.println("fine esecuzione");
+            }
+        }
+
+    
+    }
+}
